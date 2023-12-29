f345daf7febd8b66ad5fb9fef9b6a879e743dc0f
Jero327 <jeraviah@gmail.com>
2023-12-29 19:42:42 +1300

    JSON Serializer and Enable CORS

--- todo/Program.cs
+++ todo/Program.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//JSON Serializer
+builder.Services.AddControllers().AddNewtonsoftJson(options=>
+options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(
+    options=>options.SerializerSettings.ContractResolver=new DefaultContractResolver());
+
 var app = builder.Build();
+
+//Enable CORS
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
