4b7019805c03f1eba5bba2751b26be058f445685
Jero327 <jeraviah@gmail.com>
2023-12-30 16:40:24 +1300

    Expose DELETE todo API

--- todo/Controllers/TodoAppController.cs
+++ todo/Controllers/TodoAppController.cs
@@ -69,5 +69,29 @@ namespace todo.Controllers
 
             return new JsonResult("Added Successfully");
         }
+
+        [HttpDelete]
+        [Route("DeleteTodo")]
+        public JsonResult DeleteTodo(int id)
+        {
+            string query = "delete from [dbo].[todos] where id=@id";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
     }
 }
