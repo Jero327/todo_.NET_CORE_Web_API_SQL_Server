200866a478fcfe269578023d6a3150c184508864
Jero327 <jeraviah@gmail.com>
2023-12-30 13:36:54 +1300

    Expose GET todos API

---
+++ todo/Controllers/TodoAppController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public TodoAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetTodos")]
+        public JsonResult GetTodos()
+        {
+            string query = "select * from [dbo].[todos]";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
