27b95c0d25926b0dd0ef79ebdb0a926fbb7c8985
Jero327 <jeraviah@gmail.com>
2023-12-30 16:14:33 +1300

    Expose POST new todo API

--- todo/Controllers/TodoAppController.cs
+++ todo/Controllers/TodoAppController.cs
@@ -37,5 +37,37 @@ namespace todo.Controllers
 
             return new JsonResult(table);
         }
+
+        public class TodoInputModel
+        {
+            public string Details { get; set; }
+            public int Priority { get; set; }
+        }
+
+        [HttpPost]
+        [Route("AddTodo")]
+        public JsonResult AddTodo([FromBody] TodoInputModel inputModel)
+        {
+            string query = "insert into [dbo].[todos] values(@details, @priority, @completed)";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("details", inputModel.Details);
+                    myCommand.Parameters.AddWithValue("priority", inputModel.Priority);
+                    myCommand.Parameters.AddWithValue("completed", false);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
     }
 }
